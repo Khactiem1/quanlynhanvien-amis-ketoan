3a7b77e37cf2c237b6ac4ea070be9d4eeaaab017
Khactiem1 <nguyenkhactiem2k1@gmail.com>
2022-09-22 08:14:32 +0700

    add try cach api index

--- src/api/user.js
+++ src/api/user.js
@@ -1,17 +1,27 @@
 import axiosAPI from ".";
 
 export const getUserList = () => {
-  return axiosAPI.get(`/user`);
+  return axiosAPI.get(`/user`).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 export const createUserApi = (user) => {
-  return axiosAPI.post(`/user`,user);
+  return axiosAPI.post(`/user`, user).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 export const getUserApi = (id) => {
-  return axiosAPI.get(`/user/${id}`);
+  return axiosAPI.get(`/user/${id}`).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 export const editUserApi = (user) => {
-  return axiosAPI.put(`/user/${user.id}`,user);
+  return axiosAPI.put(`/user/${user.id}`, user).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
 export const deleteUserApi = (id) => {
-  return axiosAPI.delete(`/user/${id}`);
+  return axiosAPI.delete(`/user/${id}`).catch(function (error) {
+    console.log(error.toJSON());
+  });
 };
